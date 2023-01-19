d8e5fd69df1bb95ebe0cf71f11129505db9a0365
VitorOdorico <ve743137@gmail.com>
2023-01-19 13:22:03 -0300

    init to style App

--- frontend/src/components/teamplate/footer/footer.jsx
+++ frontend/src/components/teamplate/footer/footer.jsx
@@ -4,12 +4,5 @@ import React from 'react'
 
 export default  props =>
     <footer className="footer">
-        <nav className="navlist">
-            <ul>
-                <li className="links-altern">Home</li>
-                <li className="links-altern">Test</li>
-                <li className="links-altern">Competents</li>
-                <li className="links-altern">Contact-us</li>
-            </ul>
-        </nav>
+        a
     </footer>
