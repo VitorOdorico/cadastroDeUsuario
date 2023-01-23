b891377d1d70f9fb5f257507ff698af4e1e9234d
VitorOdorico <ve743137@gmail.com>
2023-01-23 10:33:18 -0300

    modifie to logo

--- frontend/src/components/teamplate/logo/logo.jsx
+++ frontend/src/components/teamplate/logo/logo.jsx
@@ -1,11 +1,11 @@
 import './logo.css';
 import React from 'react';
 
-import logo from '../../../assets/img/logo.png'
+import Logo from '../../../assets/img/logo.png'
 
 export default props=>
-    <aside className="logo">
-        <a href="/" className="logo">
-            <img src="{Logo}" alt="" />
+    <aside className="Logo">
+        <a href="/" className="Logo">
+            <img src="{Logo}" alt="iconLogo" />
         </a>
     </aside>
